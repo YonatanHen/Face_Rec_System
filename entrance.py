f8d9b300bd67aa8e3004a2005cb3faddacf23ecd
Yehonatan Hen <yehone@ac.sce.ac.il>
2019-11-24 23:46:31 +0200

    update password and user name

--- entrance.py
+++ entrance.py
@@ -1,9 +1,20 @@
 #This page open the possibilty to enter the system with password&userName
 import sqlite3
 
-#print("connect")
-db=sqlite3.connect('users.db')
-cursor=db.cursor() #cursor enable traversal over the records in database
-cursor.execute('select * from users')
-for row in cursor:
-    print(row)
+usersDB=sqlite3.connect('users.db')
+cursor=usersDB.cursor() #cursor enable traversal over the records in database
+while True:
+    username=input("Enter user-name:")
+    password=input("Enter password:")
+    cursor=usersDB.cursor() #cursor enable traversal over the records in database
+    find_user=("SELECT * FROM users WHERE username=? and password=?")
+    cursor.execute(find_user,[(username),(password)])
+    results=cursor.fetchall()
+    if results:
+        for i in results:
+            print("Welcome "+i[0]+" "+i[1])
+        break
+    else:
+        print("user-name and password not recognized,please enter again")
+
+
