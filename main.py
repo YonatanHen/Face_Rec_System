f788795fc9448a729b76c5c6bb11663477384a53
Yehonatan Hen <yehone@ac.sce.ac.il>
2019-11-25 13:17:41 +0200

    update 25.11

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import cv2
-from tkinter import *
-import keyboard
-import os
-
-print("Welcome,Please use the face-recognition system or enter password&username to log in")
-print("Enter 1 for face recognition or 2 to password&username")
-x = True
-
-while x:
-    if keyboard.is_pressed('1'):
-        os.system('C:\\Users\\your_location\\Documents\\Python_Projects\\opencvtube\\src\\faces.py')
-        x = False
-=======
 import cv2
 from tkinter import *
 import keyboard
@@ -24,9 +9,8 @@ x = True
 
 while x:
     if keyboard.is_pressed('1'):
-        os.system('C:\\Users\\יונתן\\Documents\\Python_Projects\\Face_recognition_system_project\\opencvtube\\src\\faces.py')
+        os.system('C:\\Users\\יונתן\\Documents\\Python_Projects\\Face_Rec_System\\opencvtube\\src\\faces.py')
         x = False
     if keyboard.is_pressed('2'):
-        os.system('C:\\Users\\יונתן\\Documents\\Python_Projects\\Face_recognition_system_project\\entrance.py')
+        os.system('C:\\Users\\יונתן\\Documents\\Python_Projects\\Face_Rec_System\\entrance.py')
         x = False
->>>>>>> create new folder
