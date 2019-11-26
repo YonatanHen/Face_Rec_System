961e1c8984451e2d5b8e55f6b993732f87f860fc
Yehonatan Hen <yehone@ac.sce.ac.il>
2019-11-27 01:42:10 +0200

    run background music when main.py running

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import cv2
 from tkinter import *
 import keyboard
 import os
+import pygame
+
+#playing sound in background helping with accessability for visually impaired users
+pygame.mixer.init()
+pygame.mixer.music.load('background_audio.mp3')
+pygame.mixer.music.play(999)
 
 print("Welcome,Please use the face-recognition system or enter password&username to log in")
 print("Enter 1 for face recognition or 2 to password&username:")
