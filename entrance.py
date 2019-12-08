f66f4c24d311e8065641eb2c3f2120c62b0b8ecd
Yehonatan Hen <yehone@ac.sce.ac.il>
2019-12-08 13:34:42 +0200

    update 13:33 8-12-19

--- entrance.py
+++ entrance.py
@@ -1,5 +1,6 @@
 #This page open the possibilty to enter the system with password&userName
 import sqlite3
+import datetime
 
 usersDB=sqlite3.connect('users.db')
 cursor=usersDB.cursor() #cursor enable traversal over the records in database
@@ -7,12 +8,15 @@ while True:
     username=input("Enter user-name:")
     password=input("Enter password:")
     cursor=usersDB.cursor() #cursor enable traversal over the records in database
-    find_user=("SELECT * FROM users WHERE username=? and password=?")
-    cursor.execute(find_user,[(username),(password)])
+    cursor.execute("SELECT * FROM users WHERE username=? and password=?",[(username),(password)])
     results=cursor.fetchall()
     if results:
         for i in results:
             print("Welcome "+i[0]+" "+i[1])
+            enter_time=datetime.datetime.now()
+            print("Entrance time:{0}".format(enter_time))
+            cursor.execute("UPDATE users SET entrance=? Where username=?",[(enter_time),(username)])
+            usersDB.commit()
         break
     else:
         print("user-name and password not recognized,please enter again")
