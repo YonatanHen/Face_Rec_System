8f9ef5d098d182609d116b43c9b02ef6b0fc06b4
Yehonatan Hen <yehone@ac.sce.ac.il>
2019-12-11 17:44:07 +0200

    delete user from db

--- entrance.py
+++ entrance.py
@@ -64,9 +64,14 @@ def adminMenu():
             vol=vol/100
             pygame.mixer.music.set_volume(vol)
         elif(option=='3'):
-            print("Enter the username you want to delete ")
-            usernameDel=input()
-            cursor.execute("DELETE from users WHERE username= :user",{'user':usernameDel})   
+            usernameDel=input("Enter the username that you want to delete:")
+            flag=cursor.execute("SELECT username FROM users WHERE username=?",[(usernameDel)])
+            if(flag):
+                cursor.execute("DELETE from users WHERE username=?",[(usernameDel)])
+                usersDB.commit()
+                print("User deleted successfully!")
+            else:
+                print("Username wasn't found in the database.")   
         elif(option=='4'):
             cursor.execute("select * from users")
             print(cursor)
