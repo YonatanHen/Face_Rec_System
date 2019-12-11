cd94c5be518ef2d36ed2db16a1914bc7530d146d
Yehonatan Hen <yehone@ac.sce.ac.il>
2019-12-11 19:17:08 +0200

    adding functions file+connect between faces.py to SQL

--- faces.py
+++ faces.py
@@ -7,15 +7,7 @@ from gtts import gTTS
 import os 
 from playsound import playsound
 import sqlite3
-
-
-def face_recognize(username):
-    """function check if username and password match one of the users in users.db,and return the relevant data"""
-    usersDB=sqlite3.connect('users.db')
-    cursor=usersDB.cursor()
-    cursor.execute("SELECT * FROM users WHERE username=?",[(username)])
-    return cursor.fetchall()
-
+from Functions import *
 def make_4k():
     cap.set(3, 3840)
     cap.set(4, 2160)
@@ -111,31 +103,31 @@ while(True):
                         usersDB=sqlite3.connect('users.db')
                         cursor=usersDB.cursor() #cursor enable traversal over the records in database
                         results=face_recognize(tempname)
-                        if results: #if results!=NULL, in other words, if user found in the DB
-                            for i in results:
-                                print("Time is:{0}".format(datetime.datetime.now()))
-                                if(i[7] =='no'):
-                                    print("Welcome "+i[0]+" "+i[1])
-                                    #Admin's menu
-                                    """if(i[6]=='Admin'):
-                                        option=input("Hey admin! Do you want to reach the menu? y/n")
-                                        if(option=='y' or option=='Y'):
-                                            adminMenu()
-                                        elif(option=='n' or option=='N'):
-                                            print("OK,Have a nice day!")
-                                        else:
-                                            print("I see that as 'no',Have a nice day!")"""
-                                    playsound('welcome.mp3',False)
-                                    enter_time=datetime.datetime.now().hour
-                                    cursor.execute("UPDATE users SET entrance=?,isInside='yes' WHERE username=?",[(enter_time),(tempname)])
-                                    usersDB.commit()
-                                elif(i[7]=='yes'):
-                                    print("goodbye "+i[0]+" "+i[1])
-                                    total=datetime.datetime.now().hour-int(i[4])
-                                    total=int(i[5])+total
-                                    cursor.execute("UPDATE users SET total=?,isInside='no',entrance=0 WHERE username=?",[(total),(tempname)])
-                                    usersDB.commit()
-                            break
+                        for i in results:
+                            print("Time is:{0}".format(datetime.datetime.now()))
+                            if(i[7] =='no'):
+                                print("Welcome "+i[0]+" "+i[1])
+                                #Admin's menu
+                                if(i[6]=='admin'):
+                                    option=input("Hey admin! Do you want to reach the menu? y/n:")
+                                    if(option=='y' or option=='Y'):
+                                        adminMenu()
+                                    elif(option=='n' or option=='N'):
+                                        print("OK,Have a nice day!")
+                                    else:
+                                        print("I see that as 'no',Have a nice day!")
+                                playsound('welcome.mp3',False)
+                                enter_time=float(datetime.datetime.now().hour)+(datetime.datetime.now().minute*0.01)
+                                cursor.execute("UPDATE users SET entrance=?,isInside='yes' WHERE username=?",[(enter_time),(tempname)])
+                                usersDB.commit()
+                            elif(i[7]=='yes'):
+                                print("Goodbye "+i[0]+" "+i[1])
+                                total=str(float(datetime.datetime.now().hour)+(datetime.datetime.now().minute*0.01)-(float(i[4])))
+                                total="%.2f" %(float(i[5])+float(total))
+                                total = Time_Fixer(total)
+                                cursor.execute("UPDATE users SET total=?,isInside='no',entrance=0 WHERE username=?",[(total),(tempname)])
+                                usersDB.commit()
+                            break 
 
                     if os.path.isfile("match.mp3") and tempmatch!='None' and tempmatch!=match:
                         os.remove("match.mp3")
