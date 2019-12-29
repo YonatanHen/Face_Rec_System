5879c0a87b4424f730a863737d1e8bf43fc2966c
Guy Cohen <guyco8@ac.sce.ac.il>
2019-12-29 18:46:18 +0200

    manager can add photos + delete folder of user if user is deleiting

--- Functions.py
+++ Functions.py
@@ -10,6 +10,10 @@ import os
 from playsound import playsound
 import sqlite3
 from getpass import getpass
+from time import sleep
+import tkinter as tk
+from tkinter import filedialog
+import shutil 
 
 def recognize(username,password):
     """function check if username and password match one of the users in users.db,and return the relevant data"""
@@ -62,8 +66,8 @@ def adminMenu():
     cursor=usersDB.cursor()
     print("Please choose one of the option below:")
     while(exit==False):
-        print("1.Change user data\n2.Change the volume of the system\n3.Delete user\s\n4.watch users data\n5.add new user\n6.Exit menu")
-        option=input("Enter an option:")
+        print("1.Change user data\n2.Change the volume of the system\n3.Delete user\s\n4.watch users data\n5.add new user\n6.Add new photo to an exist user\n7.Exit menu")
+        option=input("Enter an option: ")
         if(option=='1'):
             uname=input("Enter the username:")
             cursor.execute("SELECT * FROM users WHERE username=?",[(uname)])
@@ -86,7 +90,8 @@ def adminMenu():
             if(flag):
                 cursor.execute("DELETE from users WHERE username=?",[(usernameDel)])
                 usersDB.commit()
-                print("User deleted successfully!")
+                shutil.rmtree("images//"+str(usernameDel), ignore_errors=True)
+                print("User deleted successfully!\n")
             else:
                 print("Username wasn't found in the database.")   
         elif(option=='4'):
@@ -111,37 +116,79 @@ def adminMenu():
                     VALUES (?,?,?,?,?,?,?,?)""",[(fName),(lName),(uname),(password),(entrance),(total),(role),(isInside)])
                 usersDB.commit() 
                 print("Data added succesfully")
-                print("Now, take few pictures of the new worker...")
-                from time import sleep
+                print("Now, take few pictures of the new worker... press p -take a picture/q -stop Capturing")
+                
                 key = cv2. waitKey(1)
                 webcam = cv2.VideoCapture(0)
+                
                 sleep(2)
                 path="images//"+str(uname)
                 os.mkdir(path)
+                i=0
                 while True:
                     check, frame = webcam.read()
-                    cv2.imshow("Capturing", frame)
-                    i=0
+                    frame = cv2.flip(frame,1)
+                    cv2.imshow('Capturing', frame)
                     if cv2.waitKey(20) & 0xFF == ord('p'):
-                        cv2.imwrite(filename='saved_img.jpg', img=frame)
-                        webcam.release()
-                        print("Processing image...")
-                        img_ = cv2.imread('saved_img.jpg', cv2.IMREAD_ANYCOLOR)
-                        print("Converting RGB image to grayscale...")
-                        gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
-                        print("Converted RGB image to grayscale...")
-                        print("Resizing image to 100X100 scale...")
-                        img_ = cv2.resize(gray,(100,100))
-                        print("Resized...")
-                        cv2.imwrite(filename="images\\" + str(uname) + "\\" + str(uname) + str(i) +".png", img=img_)
+                        i+=1
+                        cv2.imwrite(filename="images\\" + str(uname) + "\\" + str(uname) + str(i) +".png", img=frame)
                         print("Image saved!")
+                        
                     elif cv2.waitKey(20) & 0xFF == ord('q'):
                         webcam.release()
                         cv2.destroyAllWindows()
                         break
+
         elif (option=='6'):
+            root = tk.Tk()
+            root.withdraw()
+            while(True):
+                user_fol=input("Enter username to add a picture to: ")
+                root.attributes("-topmost", True)
+                if os.path.isdir("images\\" + user_fol):
+                    add_photo=None
+                    while add_photo!='1' and add_photo!='2' and add_photo!='3':
+                        add_photo=input("For choose an exist photo press 1, to take a new photo press 2, to go back to menu press 3: ")
+                    if add_photo=='1':
+                        file_path = filedialog.askopenfilename()
+                        newPath = shutil.copy(file_path, "images\\" + user_fol + "\\" )
+                        print("\nNow the user have a new picture !\n")
+                    elif add_photo=='2':
+                        webcam = cv2.VideoCapture(0)
+                        i=0
+                        while  os.path.isfile("images\\" + user_fol + "\\" + user_fol + str(i) +".png"):
+                            i+=1
+                        i-=1
+                        while True:
+                            check, frame = webcam.read()
+                            frame = cv2.flip(frame,1)
+                            cv2.imshow('Capturing', frame)
+                            if cv2.waitKey(20) & 0xFF == ord('p'):
+                                i+=1
+                                cv2.imwrite(filename="images\\" + user_fol + "\\" + user_fol + str(i) +".png", img=frame)
+                                print("Image saved!")
+                                
+                            elif cv2.waitKey(20) & 0xFF == ord('q'):
+                                webcam.release()
+                                cv2.destroyAllWindows()
+                                sleep(2)
+                                print("\nNow the user have a new picture/s !\n")
+                                break
+                    else:
+                        break            
+                else:                    
+                    try_again=None
+                    while try_again!='y' and try_again!='n':
+                        try_again=input(("There is no folder to username - {0}, do you want to try again? y/n: ".format(user_fol)))
+                    if try_again=='n':
+                        break
+                    
+
+        elif (option=='7'):
             exit=True
             print("Exiting admin's menu...")
+            
         else:
             print("Wrong input,Enter again.")
-
+        
+        
