19d98a8d812921d29611affd9fb2e5a8d7a3d911
yarinab1 <57402773+yarinab1@users.noreply.github.com>
2020-01-01 22:14:25 +0200

    update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pygame
 import sys
 import datetime
 
+
 #playing sound in background helping with accessability for visually impaired users
 pygame.mixer.init()
 pygame.mixer.music.load('background_audio.mp3')
@@ -15,14 +16,13 @@ pygame.mixer.music.set_volume(0.4)
 
 if keyboard.is_pressed('s'):
     pygame.mixer.music.pause()
-
+os.system('window.py')
 
 
 print(datetime.datetime.now())
 x = True
 
 #Enter the system
-
 print("Welcome,Please use the face-recognition system or enter password&username to log in")
 print("Face recognition will open automatically\nEnter 'q' to exit")
 print("If you want to enter with password&username enter 2,if you regret and want to enter with face recognition,enter 1.")
