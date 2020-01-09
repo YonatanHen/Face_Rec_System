f23a6ef669e59156e013f8338ecff6650b03729c
Yehonatan Hen <yehone@ac.sce.ac.il>
2020-01-09 09:23:26 +0200

    music stops when camera opens

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ import pygame
 import sys
 import datetime
 
-
 #playing sound in background helping with accessability for visually impaired users
 pygame.mixer.init()
 pygame.mixer.music.load('background_audio.mp3')
 pygame.mixer.music.play(999)
 #set volume of background music
-pygame.mixer.music.set_volume(0.4) 
+pygame.mixer.music.set_volume(0.4)
 
 if keyboard.is_pressed('s'):
     pygame.mixer.music.pause()
+
 os.system('window.py')
-
-
 print(datetime.datetime.now())
 x = True
+
+
 
 #Enter the system
 print("Welcome,Please use the face-recognition system or enter password&username to log in")
@@ -28,15 +28,19 @@ print("Face recognition will open automatically\nEnter 'q' to exit")
 print("If you want to enter with password&username enter 2,if you regret and want to enter with face recognition,enter 1.")
 print("Stop music with 's' button")
 
-
+#music unuseful when camera open
+if pygame.mixer.music.get_busy():
+    pygame.mixer.music.pause()
 #opening face recognition system when running
 os.system('faces.py')
 
 while x:
     if keyboard.is_pressed('1'):
+        pygame.mixer.music.pause()
         os.system('faces.py')
         x = False
     if keyboard.is_pressed('2'):
+        pygame.mixer.music.pause()
         os.system('entrance.py')
         x = False
     if keyboard.is_pressed('s'):
