060ddde510b5caf94e25173b7832f3d74881bdd7
yarinab1 <57402773+yarinab1@users.noreply.github.com>
2020-01-09 17:48:03 +0200

    add font size

--- window.py
+++ window.py
@@ -20,17 +20,24 @@ def changecolor():
     i+=1
     if(i==9):
         i=0
-            
-f=25
+f=20
+def changefontsize():
+    global f
+    f+=1
+    label_1["font"] = ('Arial' , f)
+    if(f==30):
+        f=20
+    
 window=Tk()
 window.title('Face_Rec_System')
+window.geometry("1000x1000")
 label_1=Label(window,text="Welcome,Please use the face-recognition system \nor enter password&username to log in\nFace recognition will open automatically\nEnter 'q' to exit \nIf you regret and want to enter with face recognition,enter 1\nIf you want to enter with password&username enter 2\nStop music with 's' button",fg="#696969",font = ('Arial' , f))
 label_1.pack()
 Button(window, text="Open Camera Directly", command=window.destroy,font="verdana 10 bold italic").pack()
 Button(window, text="Change color", command=changecolor,font="verdana 10 bold italic").pack()
+Button(window, text="Change font size", command=changefontsize,font="verdana 10 bold italic").pack()
 window.after(15000, window.destroy)
 window.mainloop()
-
 
 
 
