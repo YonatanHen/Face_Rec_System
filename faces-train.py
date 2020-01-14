8816a156c8e84ed33d2c73dae7d29d08ea39b2ea
Yehonatan Hen <yehone@ac.sce.ac.il>
2020-01-14 14:21:03 +0200

    fix face trainer

--- faces-train.py
+++ faces-train.py
@@ -33,7 +33,7 @@ for root, dirs, files in os.walk(image_dir):
 			final_image = pil_image.resize(size, Image.ANTIALIAS)
 			image_array = np.array(final_image, "uint8")
 			#print(image_array)
-			faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
+			faces = face_cascade.detectMultiScale(image_array)
 
 			for (x,y,w,h) in faces:
 				roi = image_array[y:y+h, x:x+w]
